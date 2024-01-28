0a582e1686e2e16c81b3970b8ee24e0d504b9e6d
sadid369 <sadid.jones@gmail.com>
2024-01-28 21:43:04 +0600

    1:10 start nodejs

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'E-Commerce App',
         theme: ThemeData(
           colorScheme: const ColorScheme.light(
             primary: GlobalVariables.secondaryColor,
