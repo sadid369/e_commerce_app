df23983bcfd5beab746675443a218d3d67e0abdb
sadid369 <sadid.jones@gmail.com>
2024-02-15 22:22:07 +0600

    8:04:54  completed deal of the day api & implemented in ui

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -44,4 +44,38 @@ class HomeServices {
     }
     return productList;
   }
+
+  Future<Product> fetchDealOfDay({
+    required BuildContext context,
+  }) async {
+    Product product = Product(
+        name: '',
+        description: "",
+        quantity: 0,
+        images: [],
+        category: '',
+        price: 0);
+    try {
+      var res = await http.get(
+        Uri.parse(
+          '$uri/api/deal-of-day',
+        ),
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8",
+          'x-auth-token': context.read<UserProvider>().user.token,
+        },
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          product = Product.fromJson(res.body);
+        },
+      );
+    } catch (e) {
+      showSnackbar(context, "fetch All ${e.toString()}");
+    }
+    return product;
+  }
 }
